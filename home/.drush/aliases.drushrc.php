cfc94bed3d4fe7cd8d3bc60ee55e3dc55c584123
Spenser Jones <hello@spenserj.com>
2015-03-17 11:21:13 -0600

    Adding Drush Alias helper

---
+++ home/.drush/aliases.drushrc.php
@@ -0,0 +1,26 @@
+<?php
+
+exec('git remote -v 2> /dev/null', $remotes);
+foreach ($remotes as $remote_raw) {
+  if (strpos($remote_raw, '@') !== FALSE) { continue; }
+
+  preg_match('{\s(?<host>.*?):(?<path>(?<basename>.*?)(?:/public_html)?)(?:/.git)}', $remote_raw, $git_remote);
+
+  $host = $git_remote['host'];
+  $root = $git_remote['path'];
+  $site_name = basename($git_remote['basename']);
+  $uri = basename($site_name) . '.' . $host;
+
+  $alias_key = substr($host, 0, strpos($host, '.')) . '-' . $site_name;
+
+  $remote = array(
+    'remote-host' => $host,
+    'root' => $root,
+    'uri' => $uri,
+    'path-aliases' => array(
+      '%files' => 'sites/default/files',
+    ),
+  );
+
+  $aliases[$alias_key] = $remote;
+}
