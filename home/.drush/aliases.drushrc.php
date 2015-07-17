58a123f55aff9faccd5a72bf630bc18f79c79629
Spenser Jones <hello@spenserj.com>
2015-07-17 14:42:30 -0600

    Better support for Drush Alias names

--- home/.drush/aliases.drushrc.php
+++ home/.drush/aliases.drushrc.php
@@ -4,14 +4,14 @@ exec('git remote -v 2> /dev/null', $remotes);
 foreach ($remotes as $remote_raw) {
   if (strpos($remote_raw, '@') !== FALSE) { continue; }
 
-  preg_match('{\s(?<host>.*?):(?<path>(?<basename>.*?)(?:/public_html)?)(?:/.git)}', $remote_raw, $git_remote);
+  preg_match('{(?<name>.*?)\s+(?<host>.*?):(?<path>(?<basename>.*?)(?:/public_html)?)(?:/.git)}', $remote_raw, $git_remote);
 
   $host = $git_remote['host'];
   $root = $git_remote['path'];
   $site_name = basename($git_remote['basename']);
   $uri = basename($site_name) . '.' . $host;
 
-  $alias_key = substr($host, 0, strpos($host, '.')) . '-' . $site_name;
+  $alias_key = $git_remote['name'] . '-' . $site_name;
 
   $remote = array(
     'remote-host' => $host,
@@ -19,6 +19,7 @@ foreach ($remotes as $remote_raw) {
     'uri' => $uri,
     'path-aliases' => array(
       '%files' => 'sites/default/files',
+      '%dump-dir' => '/tmp',
     ),
   );
 
